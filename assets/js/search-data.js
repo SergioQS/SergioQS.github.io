1f1186eaa2e72ee36eb42c685fbf575dc639e5a7
SergioQS <71296519+SergioQS@users.noreply.github.com>
2025-07-31 02:57:28 +0000

    Deploying to gh-pages from @ SergioQS/SergioQS.github.io@59945f12b6dfef0adaa8b8e68c54c11d2468c972 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/mathematics/index.html";
+            window.location.href = "/mathematics/";
           },
         },{id: "nav-computation",
           title: "Computation",
