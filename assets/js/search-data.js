c2f2febab999dc0c7914bf670a96e674b0ccb9d9
SergioQS <71296519+SergioQS@users.noreply.github.com>
2025-07-31 02:58:40 +0000

    Deploying to gh-pages from @ SergioQS/SergioQS.github.io@3801af91e7b9ba5547b584cc730f29a9c5f3c30a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/other-areas/index.html";
+            window.location.href = "/other-areas/";
           },
         },{id: "nav-projects",
           title: "projects",
